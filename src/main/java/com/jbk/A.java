cbf912c98c1125286fe71821a927141c9077e46a
Sunilw018 <sunil.waghmare608@gmail.com>
2024-04-11 12:30:42 +0530

    SpringBoot AOP

--- src/main/java/com/jbk/A.java
+++ src/main/java/com/jbk/A.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class A {
 
-	@Value("${customerId}")
-	int id;
+	@Value("${customerId}") //Application.properties chi value access kara satu ahe
+	int id; // application.properties chi value use karasati
 
 	@RequestMapping("/aop")
 	public void name() {
